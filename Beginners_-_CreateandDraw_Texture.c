2589c73433d58cd6a9cf5a75602bbc86b7f3c668
Rudy Boudewijn van Etten <cromdesign@gmail.com>
2020-04-05 10:52:26 +0200

    Create Beginners_-_CreateandDraw_Texture.c

---
+++ Beginners_-_CreateandDraw_Texture.c
@@ -0,0 +1,57 @@
+
+#include "raylib.h"
+
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - generate random values");
+
+    
+     // Create a Image in memory
+    RenderTexture2D target = LoadRenderTexture(screenWidth, screenHeight);
+    
+    // Clear our texture(image) before entering the game loop
+    BeginTextureMode(target);    
+    ClearBackground(BLUE);
+    // Draw something on it.
+    DrawCircle(50,50, 10, BLACK);
+
+    EndTextureMode();
+ 
+ 
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        //----------------------------------------------------------------------------------
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+            
+ 
+            // NOTE: Render texture must be y-flipped due to default OpenGL coordinates (left-bottom)
+            DrawTextureRec(target.texture, (Rectangle){ 0, 0, target.texture.width, -target.texture.height }, (Vector2){ 0, 0 }, WHITE);
+
+            DrawText("Example of how to draw into a image/texture.", 100, 180, 40, LIGHTGRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
