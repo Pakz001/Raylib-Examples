2d86e718141cab8075c4b8d467018389dd3462c4
Rudy Boudewijn van Etten <cromdesign@gmail.com>
2020-04-12 18:52:33 +0200

    Create Beginners_-_strcpy_struct_char.c

---
+++ Beginners_-_strcpy_struct_char.c
@@ -0,0 +1,52 @@
+
+#include "raylib.h"
+#include "string.h"
+
+typedef struct book{
+    char name[20];
+}book;
+
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib example.");
+ 
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    struct book mybook;
+    
+    strcpy(mybook.name,"Title!");
+    
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        
+        
+        //----------------------------------------------------------------------------------
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+            DrawText(FormatText("Contents of name = %s",mybook.name),200,100,20,GRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+
+
+}
