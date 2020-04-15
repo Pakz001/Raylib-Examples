0069f9f44e4a04efb8a80b47244633108254c0bf
Rudy Boudewijn van Etten <cromdesign@gmail.com>
2020-04-15 17:52:41 +0200

    Update VectorForceObject.c

--- RTS/VectorForceObject.c
+++ RTS/VectorForceObject.c
@@ -17,6 +17,7 @@ typedef struct vfo{ // force vector object (repulse objects from this point.
 
 typedef struct unit{
     bool active;
+    bool controlled;
     float x,y;
     int targetx,targety;    
 };
@@ -51,13 +52,15 @@ int main(void)
     for(int y=8;y<12;y++){ // create a number of units.
     for(int x=8;x<12;x++){
         arr_unit[num].active = true;
+        arr_unit[num].controlled = false;
         arr_unit[num].x = 8*tileWidth;
         arr_unit[num].y = 8*tileHeight+GetRandomValue(-5,5);
         arr_unit[num].targetx = -1;
         arr_unit[num].targety = -1;
         num++;        
     }}
- 
+    arr_unit[0].controlled = true;
+    
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
@@ -92,15 +95,17 @@ int main(void)
             
             // Move units away from each other.
             for(int i=0;i<MAX_UNITS;i++){
+            //if(arr_unit[i].controlled)continue;
             for(int ii=0;ii<MAX_UNITS;ii++){
                 if(arr_unit[i].active==false || arr_unit[ii].active==false || i==ii)continue;
+                //if(arr_unit[ii].controlled)continue;
                 float d = distance(arr_unit[i].x,arr_unit[i].y,arr_unit[ii].x,arr_unit[ii].y);
                 if(d>64)continue;
                 float an=getangle(arr_unit[i].x,arr_unit[i].y,arr_unit[ii].x,arr_unit[ii].y);
-                arr_unit[i].x -= cos(an);
-                arr_unit[i].y -= sin(an);
-                arr_unit[ii].x += abs(cos(an));
-                arr_unit[ii].y += abs(sin(an));
+                if(arr_unit[i].controlled==false)arr_unit[i].x -= cos(an)*2;
+                if(arr_unit[i].controlled==false)arr_unit[i].y -= sin(an)*2;
+                if(arr_unit[ii].controlled==false)arr_unit[ii].x += cos(an)*2;
+                if(arr_unit[ii].controlled==false)arr_unit[ii].y += sin(an)*2;
             }
             }
             
