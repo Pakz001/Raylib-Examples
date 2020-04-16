4e53f929a456f742ce1eee572d0e702cfe411723
Rudy Boudewijn van Etten <cromdesign@gmail.com>
2020-04-16 21:32:20 +0200

    Create Beginners_-_GetScreenWidthHeight.c

---
+++ Beginners_-_GetScreenWidthHeight.c
@@ -0,0 +1,46 @@
+
+#include "raylib.h"
+
+
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib example.");
+ 
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        
+        
+        //----------------------------------------------------------------------------------
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            DrawText(FormatText("GetScreenWidth() : %i",GetScreenWidth()),100,100,20,GRAY);
+            DrawText(FormatText("GetScreenHeight() : %i",GetScreenHeight()),100,120,20,GRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+
+
+}
