da422eb847a71a47ea7c7b6042b6bb5c7f8e1c2f
Rudy Boudewijn van Etten <cromdesign@gmail.com>
2020-04-17 14:05:21 +0200

    Update bubblebobble.c

--- games/bubblebobble.c
+++ games/bubblebobble.c
@@ -1,10 +1,14 @@
+enum flag2{PLAYER1,PLAYER2,LEFT,RIGHT};
 enum flag{SCROLLLEVELDOWN,INGAME};
 
 #define MAX_PLAYERS 1
 #define MAX_FALL_SPEED 2.0f
 
+#define MAX_BUBBLES 128
+#define BUBBLE_SHOOTFORCE 10.0f
+
 #include "raylib.h"
-#include <math.h>
+#include "math.h"
 
 static const int screenWidth=800;
 static const int screenHeight=450;
@@ -16,8 +20,9 @@ static int map[512][512];
 
 typedef struct player{
     bool active;
-    int x;
-    int y;
+    float facing;
+    float x;
+    float y;
     int w;
     int h;
     bool canjump;    
@@ -26,6 +31,17 @@ typedef struct player{
 
 static struct player p[MAX_PLAYERS];
 
+typedef struct bubble{
+    bool active;
+    float x;
+    float y;
+    int state;
+    int r;
+    float mx;
+    float my;    
+}bubble;
+
+static struct bubble arr_bubble[MAX_BUBBLES];
 
 static void inilevel(void);
 static void drawmap(int offsetx,int offsety);
@@ -34,6 +50,12 @@ static void updateplayers(void);
 static bool rectsoverlap(int x1,int y1,int w1,int h1,int x2,int y2,int w2,int h2);    
 static bool playertilecollide(int num,int tile, int offsetx,int offsety);
 static void updateplayergravity();
+static void shootbubble(int player, int direction);
+static void drawbubbles(void);
+static void updatebubbles(void);
+static bool circlerectcollide(int cx,int cy,int cr, int rx, int ry,int rw,int rh);
+static bool bubbletilecollide(int num,int offsetx,int offsety);
+static float Clamp(float value, float min, float max);
     
 int main(void)
 {
@@ -54,7 +76,11 @@ int main(void)
     //--------------------------------------------------------------------------------------
     static int offsety;
     offsety = -(mapHeight*tileHeight);
-    static int gamestate=SCROLLLEVELDOWN;
+    //static int gamestate=SCROLLLEVELDOWN;
+    static int gamestate=INGAME;
+
+    arr_bubble[0].active=true;
+    arr_bubble[0].r = tileWidth/2;
     
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
@@ -68,8 +94,12 @@ int main(void)
                 updateplayers();
                 updateplayers();
                 updateplayergravity();
+                updatebubbles();
             break;
         }
+        
+        arr_bubble[0].x = GetMouseX();
+        arr_bubble[0].y = GetMouseY();
         //----------------------------------------------------------------------------------
         // Draw
         //----------------------------------------------------------------------------------
@@ -85,7 +115,11 @@ int main(void)
                     case INGAME:
                     drawmap(0,0);
                     drawplayers();
+                    drawbubbles();
                 break;
+            }
+            if(bubbletilecollide(0,0,0)){
+                DrawText(FormatText("%f",arr_bubble[0].mx),0,0,20,WHITE);
             }
         EndDrawing();
         //----------------------------------------------------------------------------------
@@ -182,6 +216,31 @@ bool playertilecollide(int num,int tile,int offsetx,int offsety){
     return false;
 }
 
+//Unit collide with solid blocks true/false
+// num = player
+bool bubbletilecollide(int num,int offsetx,int offsety){
+    if(p[num].active==false)return false;
+    int cx = (arr_bubble[num].x+offsetx)/tileWidth;
+    int cy = (arr_bubble[num].y+offsety)/tileHeight;
+    for(int y2=cy-1; y2<cy+2;y2++){//Note that the - and + are to be set differently with differently sized players
+    for(int x2=cx-1; x2<cx+2;x2++){
+        if(x2>=0 && x2<mapWidth && y2>=0 && y2<mapHeight){
+            if(map[y2][x2] > 0 ){
+                int x3 = (x2)*tileWidth;
+                int y3 = (y2)*tileHeight;
+                if(circlerectcollide(   arr_bubble[num].x+offsetx,
+                                        arr_bubble[num].y+offsety,
+                                        arr_bubble[num].r,
+                                        x3,y3,tileWidth,tileHeight)){
+                    return true;
+                }
+            }
+        }
+    }}
+    return false;
+}
+
+
 // Rectangles overlap
 bool rectsoverlap(int x1,int y1,int w1,int h1,int x2,int y2,int w2,int h2){
     if(x1 >= (x2 + w2) || (x1 + w1) <= x2) return false;
@@ -198,12 +257,14 @@ void updateplayers(void){
                                         IsKeyDown(KEY_LEFT) &&
                                         playertilecollide(num,2,-1,0)==false){
                 p[num].x-=1;
+                p[num].facing=LEFT;
             }
             if(p[num].canjump==false && 
                                         p[num].jumpforce<0 && 
                                         IsKeyDown(KEY_RIGHT) &&
                                         playertilecollide(num,2,1,0)==false){
                 p[num].x+=1;
+                p[num].facing=RIGHT;
             }
             //move left and right while falling down
             if(p[num].canjump==false && 
@@ -211,23 +272,28 @@ void updateplayers(void){
                                         IsKeyDown(KEY_LEFT) &&
                                         playertilecollide(num,99,-1,0)==false){
                 p[num].x-=1;
+                p[num].facing=LEFT;
+                p[num].facing=RIGHT;
             }
             if(p[num].canjump==false && 
                                         p[num].jumpforce>0 && 
                                         IsKeyDown(KEY_RIGHT) &&
                                         playertilecollide(num,99,1,0)==false){
                 p[num].x+=1;
+                p[num].facing=RIGHT;
             }
             //move left and right normal
             if(p[num].canjump && 
                                         IsKeyDown(KEY_LEFT) &&
                                         playertilecollide(num,99,-1,0)==false){
                 p[num].x-=1;
+                p[num].facing=LEFT;
             }
             if(p[num].canjump && 
                                         IsKeyDown(KEY_RIGHT) &&
                                         playertilecollide(num,99,1,0)==false){
                 p[num].x+=1;
+                p[num].facing=RIGHT;
             }
             
 
@@ -256,7 +322,7 @@ void updateplayergravity(){
             // Here we update the player y position.
             // We want to move 1 pixel at a time. This for collision detection(Not going through tiles.)
             for(int i=0;i<abs((int)p[num].jumpforce);i++){
-                // If we are still jumping then update the position.
+                // If we are still jumping then update the position.☺
                 if(p[num].canjump==false){
                     
                     p[num].y += p[num].jumpforce;
@@ -277,4 +343,100 @@ void updateplayergravity(){
             if(p[num].jumpforce>MAX_FALL_SPEED)p[num].jumpforce=MAX_FALL_SPEED;
         }    
     }    
+}
+
+void shootbubble(int player, int direction){
+    for(int i=0;i<MAX_BUBBLES;i++){
+        if(arr_bubble[i].active==true)continue;
+        arr_bubble[i].active = true;
+        arr_bubble[i].r = tileWidth/2;
+        if(direction==LEFT){
+            arr_bubble[i].mx=-BUBBLE_SHOOTFORCE;
+            arr_bubble[i].x = p[player].x;
+        }
+        if(direction==RIGHT){
+            arr_bubble[i].mx=BUBBLE_SHOOTFORCE;        
+            arr_bubble[i].x = p[player].x+p[player].w*2;            
+        }
+        
+        arr_bubble[i].y = p[player].y+5;
+        arr_bubble[i].state = 0;
+        return;
+    }    
+}
+
+void drawbubbles(){
+    for(int i=0;i<MAX_BUBBLES;i++){
+        if(arr_bubble[i].active==false)continue;
+        int radius = arr_bubble[i].r;
+        int x = arr_bubble[i].x;
+        int y = arr_bubble[i].y;
+        DrawCircle(x,y,radius,(Color){0,50,0,20});
+        DrawCircleLines(x,y,radius,(Color){0,100,0,100});
+        DrawCircle(x-radius+(radius/1.5),y-radius+(radius/1.5),6,(Color){200,255,200,200});
+        DrawCircle(x+(radius/2.1),y+(radius/2.1),2,(Color){200,255,200,200});
+        
+    }
+}
+
+void updatebubbles(){
+    //player(s) shoot bubbles
+    for(int i=0;i<MAX_PLAYERS;i++){
+        if(p[i].active==false)continue;
+        if(i==0){
+            if(IsKeyPressed(KEY_LEFT_ALT)){
+                if(p[i].facing==LEFT){
+                    shootbubble(i,LEFT);
+                }else{
+                    shootbubble(i,RIGHT);
+                }
+            }
+        }
+    }
+    // Bubbles movement and interaction
+    for(int i=0;i<MAX_BUBBLES;i++){
+        if(arr_bubble[i].active==false)continue;        
+        
+        for(int step=0;step<abs(arr_bubble[i].mx);step+=1){
+                  
+            if(arr_bubble[i].mx<0.0f ){
+                arr_bubble[i].x -= 1;
+                if(bubbletilecollide(i,-1,0)){
+                    arr_bubble[i].mx=0;                    
+                }
+            }
+            if(arr_bubble[i].mx>0.0f){
+                arr_bubble[i].x += 1;
+                if(bubbletilecollide(i,1,0)){
+                    arr_bubble[i].mx=0;                    
+                }
+            }
+        }
+        
+        if(arr_bubble[i].mx<0){
+            arr_bubble[i].mx+=0.2f;
+        }else{
+            arr_bubble[i].mx-=0.2f;
+        }
+        if(arr_bubble[i].mx>-0.3f && arr_bubble[i].mx < 0.3f){
+            arr_bubble[i].mx = 0;
+        }
+    }
+    
+}
+
+bool circlerectcollide(int cx,int cy,int cr, int rx, int ry,int rw,int rh){
+    float closestx = Clamp(cx, rx, rx+rw);
+    float closesty = Clamp(cy, ry, ry+rh);
+    float distancex = cx - closestx;
+    float distancey = cy - closesty;
+    float distancesquared = (distancex * distancex) + (distancey * distancey);
+    return distancesquared < (cr * cr);
+}
+
+// Clamp float value
+float Clamp(float value, float min, float max)
+{
+    const float res = value < min ? min : value;
+    return res > max ? max : res;
 }
