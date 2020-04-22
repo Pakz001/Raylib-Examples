cb2123e3454d052568e95bebe4fe758c1d0072a5
Rudy Boudewijn van Etten <cromdesign@gmail.com>
2020-04-22 07:26:40 +0200

    Create Beginners_-_String_To_Integers.c

---
+++ Beginners_-_String_To_Integers.c
@@ -0,0 +1,47 @@
+
+#include "raylib.h"
+#include "stdlib.h"
+
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib example.");
+ 
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        
+        static int num;
+        static char con[10] = "  23";
+        num = atoi(con);
+        
+        //----------------------------------------------------------------------------------
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+            DrawText(FormatText("%i",num),200,200,20,BLACK);    
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+
+
+}
