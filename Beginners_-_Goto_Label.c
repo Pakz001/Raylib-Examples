d255829772700cf3141f9459c18457d322ae3a25
Rudy Boudewijn van Etten <cromdesign@gmail.com>
2020-05-08 13:22:30 +0200

    Create Beginners_-_Goto_Label.c

---
+++ Beginners_-_Goto_Label.c
@@ -0,0 +1,58 @@
+
+#include "raylib.h"
+
+
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib example.");
+ 
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Goto label: example.
+    //
+    // With a goto you can jump out of nested loop immediately.
+    
+    
+    int i=0;
+    while(1){
+        i++;
+        if(i>1000)goto label;
+    }
+    label:{}; //notice the code block that needs to be there to work.
+
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        
+        
+        //----------------------------------------------------------------------------------
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+            
+            DrawText("Goto example..worked!",100,100,20,DARKGRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+
+
+}
