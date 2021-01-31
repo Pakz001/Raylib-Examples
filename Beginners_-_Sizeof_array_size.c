a4e44042939e52cfefcd0aa870ce958c9c5c7963
Rudy Boudewijn van Etten <cromdesign@gmail.com>
2021-01-31 22:08:33 +0100

    Create Beginners_-_Sizeof_array_size.c

---
+++ Beginners_-_Sizeof_array_size.c
@@ -0,0 +1,68 @@
+
+#include "raylib.h"
+
+void passarray(Vector2 pol[]);
+
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib example.");
+ 
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        
+        
+        //----------------------------------------------------------------------------------
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+            //
+            // Create a Vector2 array and create some points in it
+            //
+            Vector2 pol[3];
+            pol[0] = (Vector2){100,100};
+            pol[1] = (Vector2){20,300};
+            pol[2] = (Vector2){180,300};
+            //
+            // Here we pass the Vector2 array into the function.
+            passarray(pol);    
+            
+            // Here we get the array size using sizeof.
+            int size = 0;
+            size = sizeof(pol)/sizeof(pol[0]);
+    
+    
+            DrawText(FormatText("%i",size),10,10,20,BLACK);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+
+
+}
+
+//
+// Fonction that takes a array of Vector2 for use in the function.
+void passarray(Vector2 pol[]){
+    // Draw a triangle.
+    DrawTriangle(pol[0],pol[1],pol[2],RED);
+}
